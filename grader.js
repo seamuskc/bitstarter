75168a9889c3a4af46c5467b19b1dc8631cc8065
Sean Casey <ws.casey@yahoo.com>
2013-07-14 07:21:44 +0000

    test commit

--- grader.js
+++ grader.js
@@ -4,7 +4,7 @@ Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
 
-References:
+References: 
 
  + cheerio 
    - https://github.com/MatthewMueller/cheerio
